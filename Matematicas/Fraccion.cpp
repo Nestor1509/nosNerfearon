1c87f0411756fb593fe966807b9600d1be907b06
Sebastian Sierra <133537862+SebastianSierra15@users.noreply.github.com>
2023-08-27 16:24:08 -0500

    Create Fraccion.cpp

---
+++ Matematicas/Fraccion.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int mcd(int a, int b){
+    return  a? mcd(b %a, a): b;
+}
+
+struct fraccion {
+    int num, den;
+
+    fraccion(int x, int y) {
+        num = x; den = y;
+        if (num == 0) den = 1;
+        else {
+            int dividir = mcd(num, den);
+            num /= dividir;
+            den /= dividir;
+        }
+        if (den < 0){ num *= -1;  den *= -1; }
+    }
+
+    fraccion operator+(fraccion b) {//suma
+        return fraccion(num*b.den + b.num*den, den*b.den);
+    }
+    fraccion operator-(fraccion b) {//resta
+        return fraccion(num*b.den - b.num*den, den*b.den);
+    }
+    fraccion operator*(fraccion b) {//multiplicar
+        return fraccion(num*b.num, den*b.den);
+    }
+    fraccion inversa() {
+        return fraccion(den, num);
+    }
+    fraccion operator/(fraccion b) {//dividir
+        return fraccion(num*b.den, b.num*den);
+    }
+    string toString() {
+        stringstream ss;
+        ss << num;
+        if (den == 1) return ss.str();
+        ss << "/"; ss << den;
+        return ss.str();
+    }
+};
+
+int main(){
+
+    int q, w;
+    while(cin >> q >> w){
+        fraccion c(q, w);
+        cout << c.toString() << endl;
+    }
+
+    return 0;
+}
