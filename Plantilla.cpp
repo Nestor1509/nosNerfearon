3163ee629f08ad096dd30e80ff6fa1d17690e520
Nestor1509 <137181728+Nestor1509@users.noreply.github.com>
2023-10-16 21:17:17 -0500

    Update Plantilla.cpp

--- Plantilla.cpp
+++ Plantilla.cpp
@@ -1,32 +1,24 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> //
 #define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-//typedef long long ll;
-#define int long long
-#define endl '\n'
-#define ff first
-#define ss second
+#define rfor(i, n) for (int i = n; i > 0; i--)
+#define form(i, n, m) for (int i = n; i <= m; i++)
 #define forn(i, n) for (int i = 0; i < n; i++)
-#define form(i, n, m, x) for (int i = n; i < m; i += x)
-#define rfor(i, n) for (int i = n - 1; i >= 0; i--)
-#define pb push_back
+#define ms(x, n) memset(x, n, sizeof(x))
+#define vpi vector<pair<int, int>>
 #define all(x) x.begin(), x.end()
-#define sz(x) x.size()
 #define ii pair<int, int>
 #define vi vector<int>
-#define vpi vector<pair<int, int>>
-#define ms(x, n) memset(x, n, sizeof(x))
-#define find(x, n) find(all(x), n) != x.end()
-#define suma(a, b) ((a % MOD) + (b % MOD)) % MOD
-#define resta(a, b) ((a % MOD) - (b % MOD)) % MOD
-#define mult(a, b) ((a % MOD) * (b % MOD)) % MOD
-#define div(a, b) ((a % MOD) * (modularInverse(b, MOD))) % MOD
-//" \n"[i == n - 1]
+#define sz(x) x.size()
+// #define int long long
+#define ll long long
+#define ld long double
+#define pb push_back
+#define ss second
+#define endl '\n'
+#define ff first
 using namespace std;
-//comprobar limetes
-//pobrar casos de prueba diferentes
-//revisar las salidas de presetation error
-//nosNerfearon();
-
+//" \n"[i == n - 1]
+// nosNerfearon();
 signed main() {
     fast
     
