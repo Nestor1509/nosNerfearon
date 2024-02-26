f4ef80f6785851331f7c0025458533077761a021
Sebastian Sierra <133537862+SebastianSierra15@users.noreply.github.com>
2024-02-26 11:30:03 -0500

    Update Plantilla.cpp

--- Plantilla.cpp
+++ Plantilla.cpp
@@ -1,25 +1,28 @@
 #include <bits/stdc++.h> //
 #define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define rfor(i, n) for (int i = n; i > 0; i--)
+#define rfor(i, n) for (int i = n - 1; i >= 0; i--)
 #define form(i, n, m) for (int i = n; i <= m; i++)
 #define forn(i, n) for (int i = 0; i < n; i++)
-#define ms(x, n) memset(x, n, sizeof(x))
 #define vpi vector<pair<int, int>>
 #define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) x.size()
 #define ii pair<int, int>
 #define vi vector<int>
-#define sz(x) x.size()
-// #define int long long
-#define ll long long
+#define vb vector<bool>
+#define int long long int
+#define ull unsigned long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+// #define ll long long
 #define ld long double
 #define pb push_back
 #define ss second
 #define endl '\n'
 #define ff first
 using namespace std;
-//" \n"[i == n - 1]
-// nosNerfearon();
-signed main() {
+// NosNerfearon();
+signed main(){
     fast
     
     cout << flush;
